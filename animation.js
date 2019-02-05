ccfd74f1d5c58bfcf533e37862f074ea467b1434
Julien Chil Hagopian <julienchilhagopian@gmail.com>
2019-02-05 20:20:29 +0100

    back to functional program OOP wasnt justified

--- animation.js
+++ animation.js
@@ -1,43 +1,32 @@
 const catchyTitle = document.querySelector(".js-catchy-title");
 const possibleChars = "KJ*ZDZA£%D§ZEKNEZEDF£%ZJ1#LKNJK"
 
-class TextAnimation {
-  constructor() {
-    this.animation = this.changeLetters.bind(this);
-    this.show = this.show.bind(this);
-    this.onInit();
-  }
 
-  onInit() {
-    this.launch = this.animation();
-  }
-
-  changeLetters () {
+  changeLetters = () => {
     const initialText = "testing this new feature";
     let result = "";
   
     for (let i = 0; i < initialText.length + 1 ; i++) {
-      result = initialText.substr(0, i);
+    result = initialText.substr(0, i);
   
       for (let j = i; j < initialText.length; j++){
-        result += possibleChars.charAt(Math.floor(Math.random() * possibleChars.length));
+      result += possibleChars.charAt(Math.floor(Math.random() * possibleChars.length));
       }
-      this.show(result, i);
+      show(result, i);
     }
   }
 
-  show(result, i) {
+  show = (result, index) => {
     setTimeout(function(){
       catchyTitle.innerHTML = result;
-    }, i * 50);
+    }, index * 50);
   }
+  
 
-}
 
-
+// load eventListener would not working cause i wanted all pictures to be loaded before animation init 
 document.onreadystatechange = () => {
-  if (document.readyState == "complete") {
-    new TextAnimation();
+  if (document.readyState === "complete") {
+    changeLetters();
   }
 };
-
