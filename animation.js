4660e500e5c9be3664de73ec9b8945abc50306dc
Julien Chil Hagopian <julienchilhagopian@gmail.com>
2019-02-06 19:33:42 +0100

    new media query for giant screen

--- animation.js
+++ animation.js
@@ -23,7 +23,6 @@ const catchyTitle = document.querySelector(".js-catchy-title");
   }
 
   
-  //window.addEventListener("load", heightCalulator);
 // load eventListener would not work cause i wanted all pictures to be loaded before animation init 
 document.onreadystatechange = () => {
   if (document.readyState === "complete") {
@@ -36,31 +35,24 @@ document.onreadystatechange = () => {
 // Based on most used screen resolution in france
 function heightCalulator(element) {
   let winWidth = window.innerWidth;
-  console.log(winWidth);
   switch(true){
     case winWidth <= 375:
     catchyTitle.style.height = `${winWidth + 140}px`;
-    console.log(1);
     break;
     case winWidth <= 768:
     catchyTitle.style.height = `${winWidth + 80}px`;
-    console.log(2);
     break;
     case winWidth <= 1160:
     catchyTitle.style.height = `${winWidth - 60}px`;
-    console.log(3);
     break;
     case winWidth <= 1366:
     catchyTitle.style.height = `${winWidth - 235}px`;
-    console.log(5);
     break;
     case winWidth <= 1500:
     catchyTitle.style.height = `${winWidth - 535}px`;
-    console.log("test");
     break;
     case winWidth <= 1920:
     catchyTitle.style.height = `${winWidth - 1085}px`;
-    console.log(6);
     break;
   }
 }
